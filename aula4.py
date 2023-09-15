cba9ea91369475f82090a5643fbda35979d9be33
MarcosSouzaa <marcossouza.git@gmail.com>
2023-09-15 20:38:14 -0300

    aula 4 Tipos primitivos int, float e type

---
+++ aula4.py
@@ -0,0 +1,21 @@
+# Tipos primitivos int 'Inteiros' e float 'flutuantes' 
+
+# int -> número inteiro
+# O tipo int representa qualquer número
+# Positivo ou negativo. int sem sinal é considerado positivo
+print(11) #int
+print(-11) #int
+print(0)
+
+# float -> Número com ponto flutuante
+# O tipo float representa qualquer número
+# positivo ou negativo com ponto flutuante
+# float sem sinal é considerado positivo
+print(1.1, 10.11)
+print(0.0, -1.5)
+
+# A função type mostra o tipo que o python inferiu ao valor, embora type seja uma CLASSE
+print(type('Otávio'))
+print(type(1))
+print(type(1.55))
+print(type('calça'), type(1544), type(-2.599), type(0.00))
