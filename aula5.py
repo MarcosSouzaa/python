fb094d5bd7d113c024924e45717c325c4aadc2d6
MarcosSouzaa <marcossouza.git@gmail.com>
2023-09-15 21:55:23 -0300

    Tipos de dados boolean

---
+++ aula5.py
@@ -0,0 +1,9 @@
+# Tipo de dados bool 'Boolean'
+# Ao questionar algo em um programa, só existem duas respostas possíveis:
+# True ou false
+''' Existem vários operadores para questionar, dentre eles o ==, que é um operador lógico 
+que questiona se um valor é igual ao outro'''
+
+print(10 == 10) # Sim => True => verdadeiro
+print(10 == 11) # Não => False => Falso
+print(type(10 == 10))
