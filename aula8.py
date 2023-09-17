d91ebc92cdafee142ddf4a97c836daa04367a835
MarcosSouzaa <marcossouza.git@gmail.com>
2023-09-17 20:33:12 -0300

    Exercícios com variáveis

---
+++ aula8.py
@@ -0,0 +1,13 @@
+nome = 'Marcos Antonio'
+sobrenome = 'Silva de Souza'
+idade = 56
+ano_de_nascimento = 2023 - idade
+maior_de_idade = idade >= 18
+altura_metros = float(187/100)
+
+print('Nome:', nome)
+print('Sobrenome:', sobrenome)
+print('Idade:', idade, 'anos')
+print('Ano de Nascimento:', ano_de_nascimento)
+print('É maior de idade?', maior_de_idade)
+print('Altura em metros:', altura_metros,'m' )
