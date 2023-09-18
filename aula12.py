a3de24259d277b2405153d1e75c66af04a603391
MarcosSouzaa <marcossouza.git@gmail.com>
2023-09-18 19:26:31 -0300

    Exercício Cálculo do IMC

---
+++ aula12.py
@@ -0,0 +1,13 @@
+# Cálculo do IMC
+# IMC = Peso ÷ (Altura × Altura)
+# IMC = 80 kg ÷ (1,80 m × 1,80 m) = 24,69 kg/m2 (Peso ideal)
+
+nome = 'Marcos Souza'
+altura = 1.80
+peso = 95
+imc = ... # placeholder como um código que não foi escrito. Posso usar para escrever o código depois 
+imc = peso / altura ** 2
+
+print(nome, 'tem', altura, 'de altura,',)
+print('pesa', peso, 'quilos e seu imc é',)
+print(imc)
