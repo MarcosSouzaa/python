8f9b5d9d45e06c028e47a6ff8b7d994ea44b3121
MarcosSouzaa <marcossouza.git@gmail.com>
2023-09-18 16:25:51 -0300

    Concatenação

---
+++ aula10.py
@@ -0,0 +1,18 @@
+# Concatenação
+concatenacao = 'A' + 'B' + 'C'
+print(concatenacao) #imprime ABC
+
+# concatenando com espaço
+nome = 'Marcos' + ' ' + 'Souza'
+print(nome) 
+
+# para concatenar string com o número, vou precisar colocar o tipo do número com str
+nome = 'Marcos ' + str(56) + ' ' + 'Souza'
+print(nome)
+
+# para fazer a repetição, posso usar o sinal * multiplicação
+a_dez_vezes = 'A' * 10
+print(a_dez_vezes)
+
+tres_vezes_marcos = 'Marcos' * 3
+print(tres_vezes_marcos)
