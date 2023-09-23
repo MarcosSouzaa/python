6466a85a3febaa8cd8fd91d08bf1e8c92686935b
MarcosSouzaa <marcossouza.git@gmail.com>
2023-09-23 15:46:02 -0300

    Debugger do VS Code e o interpretador do Python lendo os códigos

---
+++ aula18.py
@@ -0,0 +1,20 @@
+# Debugger do VS Code e o interpretador do Python lendo os códigos
+# Usando o step Over indo um passo de cada vez
+condicao1 = False
+condicao2 = False
+condicao3 = True
+condicao4 = True
+
+if condicao1:
+    print('Código para condição 1')
+    print('Repetindo código da condição 1')
+elif condicao2:
+    print('Código para condição 2')
+elif condicao3:
+    print('Código para condição 3')
+elif condicao4:
+    print('Código para condição 4')
+else:
+    print('Nenhuma condição foi satisfeita')
+
+print('Foi impresso pois está fora do if')
